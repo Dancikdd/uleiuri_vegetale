9aec90ffe523040619518dc55917738ee002d9f4
Dancik <daniel.ursan525@gmail.com>
2026-09-05 08:54:49 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    $denumireProiect = "Sistem de gestionare a unui magazin de uleiuri vegetale";
+    $autor = "Ursan Daniel";
+    $grupa = "PAPP-231";
+    $entitate = "Student";
+    $descriere = "O aplicatie care va gestiona produsele ca uleiuri vegetale de toate tipurile";
