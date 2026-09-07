fa57e0e3f280c2693e64e70e787ca35425e2a799
Dancik <daniel.ursan525@gmail.com>
2026-09-07 10:47:56 +0300

    Sarcina 2 complete

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+ // Sarcina 1
     $denumireProiect = "Sistem de gestionare a unui magazin de uleiuri vegetale";
     $autor = "Ursan Daniel";
     $grupa = "PAPP-231";
     $entitate = "Student";
     $descriere = "O aplicatie care va gestiona produsele ca uleiuri vegetale de toate tipurile";
+?>
+
+    <?php
+    echo "<h1>$denumireProiect</h1>";
+    echo "<p><strong>Autor:</strong> $autor</p>";
+    echo "<p><strong>Grupa:</strong> $grupa</p>";
+    echo "<p><strong>Entitate:</strong> $entitate</p>";
+    echo "<p><strong>Descriere:</strong> $descriere</p>";
+    ?>
