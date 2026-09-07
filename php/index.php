b5530d2455a8102e658637a0137a69162998681a
Dancik <daniel.ursan525@gmail.com>
2026-09-07 11:48:44 +0300

    Sarcinile 4 si 5 complete

--- php/index.php
+++ php/index.php
@@ -9,12 +9,24 @@
     
 //Sarcina 3
    $denumireUlei = "Ulei de floarea soarelui";
-   $cantitateStoc = 10;
-   $tipUlei = "Ulei vegetal";
-   $pretPeLitru = 30.99;
+   $cantitateStoc = 20;
+   $tipUlei = "rafinat";
+   $pretPeLitru = 35.99;
    $esteBio = true;
+// Sarcina 4
+    const TVA = 8;
+    const STOC_MINIM_ALERTA = 5;
 
+//Sarcina 5
+    $pretCuTVA = $pretPeLitru + ($pretPeLitru * TVA / 100);
+    $valoareStoc = $cantitateStoc * $pretPeLitru;
 
-   
+    $cantitatePesteMinim = ($cantitateStoc > STOC_MINIM_ALERTA)
+        ? $cantitateStoc - STOC_MINIM_ALERTA
+        : 0;
+
+    $necesitaReaprovizionare = ($cantitateStoc <= STOC_MINIM_ALERTA);
 ?>
+
+
 
